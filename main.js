f1f8fa0eb21b807e01c559da5d3805f225a08251
omar <omerragb55@gmail.com>
2023-12-19 16:10:20 +0200

    upload up scroll

--- main.js
+++ main.js
@@ -20,3 +20,24 @@ function updateCountdown() {
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
     time--;
 }
+
+
+// up scroll
+
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+    if (this.scrollY >= 800) {
+        span.classList.add("show");
+    }else {
+        span.classList.remove("show");
+    }
+}
+
+
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
